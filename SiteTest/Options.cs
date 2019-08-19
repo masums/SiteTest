2055d91f57833b658db119e1662378a132b5ddd5
masum <masums@gmail.com>
2019-08-19 18:23:36 +0600

    initial commit

---
+++ SiteTest/Options.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpAttacker
+{
+    public sealed class Options
+    {
+        [Option('u', "url", Required = true, HelpText = "Website URL https://osl.one")]
+        public string Url { get; set; }
+
+        [Option('m', "method", Required = true, HelpText = "Request method GET/POST", Default = "GET")]
+        public string Method { get; set; }
+
+        [Option('q', "querystring", Required = false, HelpText = "& seperated query string")]
+        public string QueryString { get; set; }
+
+        [Option('a', "agent", Required = false, HelpText = "Number of concurrent request agent", Default = 1)]
+        public int AgentNumber { get; set; }
+
+        [Option('n', "request", Required = false, HelpText = "Number of request per agent", Default = 100)]
+        public int NumberOfRequest{ get; set; }
+    }
+}
---
+++ SiteTest/Program.cs
@@ -0,0 +1,94 @@
+﻿using CommandLine;
+using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpAttacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Site Tester");
+
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(opts => HandleCommand(opts))
+                .WithNotParsed((errs) => HandleParseError(errs));
+
+            Console.WriteLine("Operation Completed");
+
+            Console.ReadKey();
+        }
+
+
+        private static void HandleCommand(Options opts)
+        {
+            Console.WriteLine($"URL : {opts.Url})");
+            Parallel.For(0, opts.AgentNumber, (current) =>
+            {
+                Console.WriteLine($"Aget: {current}");
+                
+                for (int i = 0; i < opts.NumberOfRequest; i++)
+                {
+                    Console.WriteLine($"Agent {current}, Request {i}: Start");
+                    try
+                    {
+                        if (opts.Method.Equals("POST", StringComparison.OrdinalIgnoreCase))
+                        {
+                            var request = HttpWebRequest.Create(opts.Url);
+                            request.Method = opts.Method.ToUpper();
+
+                            using (var reqStream = request.GetRequestStream())
+                            {
+                                if (string.IsNullOrWhiteSpace(opts.QueryString) == false)
+                                {
+                                    reqStream.Write(Encoding.UTF8.GetBytes(opts.QueryString));
+                                }
+                            }
+
+                            var rsp = request.GetResponse();
+                            if (rsp != null)
+                            {
+                                using (var sr = new StreamReader(rsp.GetResponseStream()))
+                                {
+                                    Console.WriteLine(sr.ReadToEnd());
+                                }
+                            }
+                        }
+                        else
+                        {
+                            var request = HttpWebRequest.Create(opts.Url + "?" + opts.QueryString);
+                            request.Method = opts.Method.ToUpper();
+                            var rsp = request.GetResponse();
+                            if (rsp != null)
+                            {
+                                using (var sr = new StreamReader(rsp.GetResponseStream()))
+                                {
+                                    Console.WriteLine(sr.ReadToEnd());
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Agent {current}, Request {i}: Error: {ex.Message}" );
+                    }
+                    Console.WriteLine($"Agent {current}, Request {i}: End");
+                }
+            });
+        }
+
+        private static void HandleParseError(IEnumerable errs)
+        {
+            Console.WriteLine("Command Line parameters provided were not valid!");
+            foreach (var item in errs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
